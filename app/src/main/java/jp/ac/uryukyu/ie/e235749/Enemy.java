fbadc1d13ad84a3ef868cdec5a3031488f20dcc5
Kaneku Ren <e235749@ie.u-ryukyu.ac.jp>
2024-11-19 21:30:50 +0900

    second commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235749/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235749/Enemy.java
@@ -34,10 +34,22 @@ public class Enemy {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
+
+    //変更前
+    // public void attack(Hero hero){
+    //     int damage = (int)(Math.random() * attack);
+    //     System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+    //     hero.wounded(damage);
+    // }
+
     public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
+        if(dead){
+            System.out.println("enemy自身が死亡している状態では攻撃できない");
+        }else{
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
